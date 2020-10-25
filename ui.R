e23d8ded0f2a7726816fea41c93ef78e64296052
Mowniesh Asokan <71217867+mowas455@users.noreply.github.com>
2020-10-25 21:26:04 +0530

    Delete ui.R
    ui deleted

--- ui.R
+++
@@ -1,9 +0,0 @@
-ui <- fluidPage(titlePanel("Clinical Data"),
-                sidebarLayout(
-                  sidebarPanel(
-                     selectInput("variable","Variables:", choices = colnames(dataSet))),
-                  mainPanel(
-                    plotOutput("barplot1"),
-                    plotOutput("barplot2"),
-                    plotOutput("plot"))
-                ))
